37ed90296ab50e73796f96667f097ac8fa69f90b
J34nPC24 <jean.cotera@epn.edu.ec>
2022-10-04 10:59:30 -0500

    Trabajo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nombre = "Infrastructure as a service ";
+
+        String acronimo = obtenerAcronimo(nombre);
+
+        System.out.println(acronimo);
+    }
+
+    private static String obtenerAcronimo(String nombre) {
+        String[] palabrasSeparadas = separarPalabras(nombre);
+        String acronimo = "";
+        for (String palabra : palabrasSeparadas) {
+            if (palabra.length() < 3) {
+                acronimo += palabra.toLowerCase(Locale.ROOT).charAt(0);
+            } else {
+                acronimo += palabra.toUpperCase(Locale.ROOT).charAt(0);
+            }
+        }
+        return acronimo;
+    }
+
+    private static String[] separarPalabras(String nombre) {
+        String[] palabrasSeparadasPorEspaciosEnBlanco = nombre.split(" ");
+
+        ArrayList<String> palabrasSeparadasPorGuion = new ArrayList<>();
+
+        for (String palabra : palabrasSeparadasPorEspaciosEnBlanco) {
+            String[] aux = palabra.split("-");
+            palabrasSeparadasPorGuion.addAll(Arrays.asList(aux));
+        }
+
+        return palabrasSeparadasPorGuion.toArray(new String[0]);
     }
 }
